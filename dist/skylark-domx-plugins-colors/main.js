242d9cd032258e9a2d4d9476343a91ce180a4ef0
hudaokeji <github@hudaokeji.com>
2021-08-30 00:41:08 +0900

    rename module file name

--- dist/skylark-domx-plugins-colors/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * skylark-domx-plugins-colors - The skylark color plugin library
- * @author Hudaokeji, Inc.
- * @version v0.9.0
- * @link https://github.com/skylark-domx-plugins/skylark-domx-plugins-colors/
- * @license MIT
- */
-define(["skylark-domx-query","./colors","./colorer","./ColorPalette","./ColorPicker","./ColorPane","./Gradienter"],function(r,o,e){return o});
-//# sourceMappingURL=sourcemaps/main.js.map
